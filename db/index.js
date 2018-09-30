fb4f6f21633d4df94d75d739a02b393bf2eb7fa1
Mihelle Torres <lic.nancy.torres@gmail.com>
2018-09-29 23:07:30 -0500

    Linter happiness =D

--- db/index.js
+++ db/index.js
@@ -38,10 +38,7 @@ class DB {
   insert(table, resource) {
     return new Promise((resolve, reject) => {
       this.connection.query('INSERT INTO ?? SET ?', [table, resource], (error, results) => {
-        if (error) {
-          let err = this.processError(error);
-          return reject(err);
-        }
+        if (error) return reject(this.processError(error));
         return resolve(results);
       });
     });
@@ -57,31 +54,34 @@ class DB {
 
   processError(err) {
     const error = {};
+    let data;
 
     switch (err.code) {
       case 'ER_DUP_ENTRY':
-        let data = this.getDataFromErrorMsg(err.sqlMessage);
-        error['duplicated'] = {
+        data = this.getDataFromErrorMsg(err.sqlMessage);
+        error.duplicated = {
           message: `The ${data.field} ${data.data} already exists on the system`,
           field: data.field,
           sql: err.sql,
         };
         break;
       default:
-
+        error.error = {
+          message: err.sqlMessage,
+          sql: err.sql,
+        };
     }
 
     return error;
   }
 
   getDataFromErrorMsg(message) {
-    let data = unescape(message).match(/'([^']+)'/g)
+    const data = unescape(message).match(/'([^']+)'/g);
     return {
-      field: data[1].slice(1,-1),
-      data: data[0].slice(1,-1),
-    }
+      field: data[1].slice(1, -1),
+      data: data[0].slice(1, -1),
+    };
   }
-
 }
 
 module.exports = new DB();
