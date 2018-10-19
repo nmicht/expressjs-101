d94525008182e0e8420c08e7702b62aa085df57e
Mihelle Torres <lic.nancy.torres@gmail.com>
2018-10-18 23:43:00 -0500

    Ejemplo de carga de archivos con multer

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+
+function eraseFiles(req) {
+  // Iterate over req.file, req.files and req.fields to remove all the files
+  if (req.file) {
+    fs.unlink(req.file.path);
+  }
+}
+
 function errorHandler(err, req, res, next) {
+  // In case something fail, will be great to erase the uploaded files
+  eraseFiles(req);
   console.error('Error handler', err);
   return res.status(err.status || 500).send(err);
 }
